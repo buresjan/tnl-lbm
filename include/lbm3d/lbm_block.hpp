53685621e5cfd84db82a3dc995a6b237b63ba1a7
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2022-12-07 17:04:01 +0100

    lbm3d_mpi_new: update LBM_BLOCK::setEqLat to avoid compiler warnings with new TNL

--- include/lbm3d/lbm_block.hpp
+++ include/lbm3d/lbm_block.hpp
@@ -43,8 +43,16 @@ LBM_BLOCK<CONFIG>::LBM_BLOCK(const TNL::MPI::Comm& communicator, idx3d global, i
 template< typename CONFIG >
 void LBM_BLOCK<CONFIG>::setEqLat(idx x, idx y, idx z, real rho, real vx, real vy, real vz)
 {
-	for (uint8_t dfty=0; dfty<DFMAX; dfty++)
-		CONFIG::COLL::setEquilibriumLat(hfs[dfty], x, y, z, rho, vx, vy, vz);
+	for (uint8_t dfty=0; dfty<DFMAX; dfty++) {
+		// shift global indices to local
+		const auto local_begins = hfs[dfty].getLocalBegins();
+		const idx lx = x - local_begins.template getSize< 0 >();
+		const idx ly = y - local_begins.template getSize< 1 >();
+		const idx lz = z - local_begins.template getSize< 2 >();
+		// call setEquilibriumLat on the local array view
+		auto local_view = hfs[dfty].getLocalView();
+		CONFIG::COLL::setEquilibriumLat(local_view, lx, ly, lz, rho, vx, vy, vz);
+	}
 }
 
 template< typename CONFIG >
