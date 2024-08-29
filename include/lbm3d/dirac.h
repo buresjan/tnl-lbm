a5484dcfb3e2004fa3ab3d978868445867542dd8
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2024-08-29 19:18:23 +0200

    Refactored Dirac delta functions

--- include/lbm3d/dirac.h
+++ include/lbm3d/dirac.h
@@ -1,67 +1,66 @@
 #pragma once
+
 #include "defs.h"
 
-
-//TODO: Missing default case
 template< typename real >
 CUDA_HOSTDEV bool isDDNonZero(int i, real r)
 {
 	switch (i)
 	{
 		case 1: // VU: phi3
-			if(fabs(r) < (real)1.0)
+			if (fabs(r) < (real)1.0)
 				return true;
 			else
 				return false;
 		case 2: // VU: phi2
-			if(fabs(r) < (real)2.0)
+			if (fabs(r) < (real)2.0)
 				return true;
 			else
 				return false;
 		case 3: // VU: phi1
-			if (fabs(r)>=(real)2.0)
+			if (fabs(r) >= (real)2.0)
 				return false;
 			else
 				return true;
 		case 4: // VU: phi4
-			if (fabs(r)>=(real)1.5)
+			if (fabs(r) >= (real)1.5)
 				return false;
 			else
 				return true;
 	}
 	return false;
 }
-//TODO: Convert numbers to (reals)
+
+
 //TODO: Remove unnecessary isDDNonZero checks
 template< typename real >
 CUDA_HOSTDEV real diracDelta(int i, real r)
 {
 	if(!isDDNonZero(i, r))
 	{
-		//fmt::print("A / warning: zero Dirac delta: type={}\n", i);
 		return 0;
 	}
 	else{
 		switch (i)
 		{
 			case 1: // VU: phi3
-				return (1.0 - fabs(r));
+				return 1 - fabs(r);
 			case 2: // VU: phi2
-				return 0.25*((1.0+cos((PI*r)/2.0)));
+				return (real)0.25 * (1 + cos((real)PI*r*(real)0.5));
 			case 3: // VU: phi1
-				if (fabs(r)>1.0)
-					return (5.0 - 2.0*fabs(r) - sqrt(-7.0 + 12.0*fabs(r) - 4.0*r*r))/8.0;
+				if (fabs(r) > (real)1.0)
+					return (5 - 2*fabs(r) - sqrt(-7 + 12*fabs(r) - 4*r*r)) / (real)8.0;
 				else
-					return (3.0 - 2.0*fabs(r) + sqrt(1.0 + 4.0*fabs(r) - 4.0*r*r))/8.0;
+					return (3 - 2*fabs(r) + sqrt(1 + 4*fabs(r) - 4*r*r)) / (real)8.0;
 			case 4: // VU: phi4
-				if (fabs(r)>0.5)
-					return (5.0 - 3.0*fabs(r) - sqrt(-2.0+6.0*fabs(r)-3.0*r*r))/6.0;
+				if (fabs(r) > (real)0.5)
+					return (5 - 3*fabs(r) - sqrt(-2 + 6*fabs(r) - 3*r*r)) / (real)6.0;
 				else
-					return (1.0 + sqrt(1.0 - 3.0*r*r))/3.0;
+					return (1 + sqrt(1 - 3*r*r)) / (real)3.0;
 		}
 	}
-	//Just a backup return if something doesn't return 0
-	//fmt::print("warning: zero Dirac delta: type={}\n", i);
+
+	// Just a failsafe in case something does not return 0
 	return 0;
 }
 
@@ -97,7 +96,6 @@ CUDA_HOSTDEV typename LLVectorType::ValueType::Real calculate3Dirac(int rDirac, 
 	real d1; //dirac 1
 	real d2; //dirac 2
 	real d3; //dirac 3
-	real ddd;
 
 	d1 = diracDelta(rDirac,(LL[rowIndex].x - LL[colIndex].x));
 	if (d1>0)
@@ -108,13 +106,9 @@ CUDA_HOSTDEV typename LLVectorType::ValueType::Real calculate3Dirac(int rDirac, 
 			d3=diracDelta(rDirac, (LL[rowIndex].z - LL[colIndex].z));
 			if (d3>0)
 			{
-				ddd = d1*d2*d3;
-				//fmt::print("Dirac result: {}  \n", ddd);
-				return ddd;
+				return d1*d2*d3;
 			}
-
 		}
-
 	}
 	return 0;
 }
