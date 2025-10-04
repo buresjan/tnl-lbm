582a3f779531444b908cddf4dadbb66533874c0e
Jan Bureš <bures.work@gmail.com>
2025-10-04 19:39:01 +0200

    update job

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=lbm-batch
-#SBATCH --time=3-23:59:59
+#SBATCH --time=48:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2G
 #SBATCH --output=lbm_batch.out
@@ -8,7 +8,6 @@
 
 set -euo pipefail
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$ROOT_DIR"
+cd "$SLURM_SUBMIT_DIR"
 
 python3 run_all_geometries.py --start 0 --end 180 --type1-bouzidi on
